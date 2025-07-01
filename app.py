a5459ae839a26ba6cbbd698db6131dedeb59894e
Iput Taufiqurrohman Suwarto <taufiqurrohman.suwarto@gmail.com>
2025-07-01 11:44:12 +0700

    add port

--- app.py
+++ app.py
@@ -34,4 +34,4 @@ def remove_background():
         return jsonify({'error': str(e)}), 400
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    app.run(debug=True, host='0.0.0.0', port=5000)
